0548add25c92dae55af9f8fbeff764ad36deb9ff
rishabh2609 <rishabhchanana97@gmail.com>
2017-06-07 10:26:09 -0700

    sitemap changes

--- website/js/carousel.js
+++ website/js/carousel.js
@@ -1,23 +1,23 @@
 $('#but1').click( function() {
-	$('#but1').addClass('active');
-	$('#but2').removeClass('active');
-    $('#but3').removeClass('active');
+	$('#but1').addClass('selected');
+	$('#but2').removeClass('selected');
+    $('#but3').removeClass('selected');
     $("#car-item1").animate({opacity: "1"}, 150);
     $("#car-item2").animate({opacity: "0"}, 150);
     $("#car-item3").animate({opacity: "0"}, 150);
 });
 $('#but2').click( function() {
-	$('#but2').addClass('active');
-	$('#but1').removeClass('active');
-    $('#but3').removeClass('active');
+	$('#but2').addClass('selected');
+	$('#but1').removeClass('selected');
+    $('#but3').removeClass('selected');
     $("#car-item1").animate({opacity: "0"}, 150);
     $("#car-item2").animate({opacity: "1"}, 150);
     $("#car-item3").animate({opacity: "0"}, 150);
 });
 $('#but3').click( function() {
-	$('#but3').addClass('active');
-	$('#but2').removeClass('active');
-    $('#but1').removeClass('active');
+	$('#but3').addClass('selected');
+	$('#but2').removeClass('selected');
+    $('#but1').removeClass('selected');
     $("#car-item1").animate({opacity: "0"}, 150);
     $("#car-item2").animate({opacity: "0"}, 150);
     $("#car-item3").animate({opacity: "1"}, 150);
